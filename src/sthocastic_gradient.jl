5f95cb2cd49f50cebd38d979ceac2a47de6e7b8b
Rogério Mainardes <r.otavioms@gmail.com>
2020-10-05 18:25:11 -0300

    [WIP] Hyper parameters organized

--- src/sthocastic_gradient.jl
+++ src/sthocastic_gradient.jl
@@ -7,49 +7,79 @@ export sthocastic_gradient
 
 function sthocastic_gradient(
     nlp::AbstractMultObjModel;
-    γ = 1e-2,
-    max_eval=10000,
-    max_time = 60.0,
-    atol = 1e-8,
-    rtol = 1e-8
+    learning_rate::Symbol = :optimal, 
+    γ::Float64 = 1e-2, 
+    α::Float64 = 1e-2, # for penalty
+    ρ::Float64 = 0.85, # just for elasticnet
+    penalty::Symbol = :l2, 
+    max_eval::Int=50,
+    max_time::Float64 = 60.0,
+    atol::Float64 = 1e-8,
+    rtol::Float64 = 1e-8,
+    power_t::Float64 = 1e-2
   )
 
-  start_time = time()
   iter = 0
+  start_time = time()
   β = nlp.meta.x0
+  βavg = similar(β) # test later
   n = nlp.meta.nobj
   g = similar(β)
-
-  # f = nlp.ℓ(nlp.y[i], nlp.h(nlp.X[i,:],β))
-  # N = norm(nlp.ℓ(nlp.y, nlp.h(nlp.X,β)))
-
-  # ϵt = atol + rtol*N
+  f = NLPModels.obj(nlp, β)
+  NLPModels.grad!(nlp,β,g)
+  
   Δt = time() - start_time
-  # solved = N < ϵt
-  tired = (Δt > max_time) || (iter > max_eval)
+  
+  tired = (Δt > max_time) 
+  itermax = iter > max_eval
+  
+  @info log_header([:iter, :f],
+                   [Int, Float64],
+                   hdr_override=Dict(:f => "f(β)"))
 
-  # @info log_header([:iter, :f],
-  #                  [Int, Float64],
-  #                  hdr_override=Dict(:f => "f(β)"))
+  @info log_row(Any[iter, f])
 
-  # @info log_row(Any[iter, f])
+  
+  P = if penalty == :l2
+    β -> β 
+  elseif penalty == :l1
+    β -> sign.(β)
+  elseif penalty == :elasticnet
+    β -> ρ * β + (1-ρ) * sign.(β)
+  end
+  
 
-  for k = 1:30n # I got better results with this number
-    for i in shuffle(1:n)
-      β -= γ * grad!(nlp, i, β, g) 
+  for k = 1:max_eval
+  # while !(γ < 0.01 || tired) # sounds good. Doesnt work
+
+    if learning_rate == :optimal
+      γ = 1/(α * (1e3 + iter)) 
+    elseif learning_rate == :invscaling 
+      γ = 1e-2 / (iter+1)^(power_t)
+    elseif learning_rate == :constant
+      γ = γ    
     end
 
+    βavg .= 0 # test later
+    for i in shuffle(1:n)
+      β -= γ * (α * P(β) + grad!(nlp, i, β, g))
+      βavg += β # test later
+    end
+    βavg = βavg/n # test later
+
     Δt = time() - start_time
+    tired = (Δt > max_time) 
+    solved = (γ < 1e-2)
+    itermax = iter > max_eval
     iter+=1
-    tired = (Δt > max_time) || (iter > max_eval)
   end
 
-  status = if (iter == 30n)
+  status = if (iter == max_eval)    
     :first_order
   elseif tired
     if Δt >: max_time
       :max_time
-    else
+    elseif itermax 
       :max_eval
     end
   else
@@ -58,7 +88,8 @@ function sthocastic_gradient(
 
   return GenericExecutionStats(status, nlp;  
                                 solution = β,
-                                #= objective=f, =#
+                                solver_specific=Dict(:βavg=>βavg),
                                 elapsed_time=Δt,
-                                iter=iter)
+                                iter=iter
+                                )
 end
