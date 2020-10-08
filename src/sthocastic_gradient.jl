df0995470c2378d90f0b2d15f2563d3e66773945
Rogério Mainardes <63982716+RogerioOMDS@users.noreply.github.com>
2020-10-08 11:52:50 -0300

    Update src/sthocastic_gradient.jl
    Co-authored-by: Abel Siqueira <nepper271@gmail.com>

--- src/sthocastic_gradient.jl
+++ src/sthocastic_gradient.jl
@@ -49,7 +49,6 @@ function sthocastic_gradient(
   
 
   for k = 1:max_eval
-  # while !(γ < 0.01 || tired) # sounds good. Doesnt work
 
     if learning_rate == :optimal
       γ = 1 / (α * (1e3 + iter)) 
